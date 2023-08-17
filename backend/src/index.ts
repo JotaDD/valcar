9717a48c48c23d0bafd6f2a632ec3ef2b2bed338
Joao Gomes <joaoeduardogomes@gmail.com>
2023-08-17 13:30:49 -0300

    feat: add express basic server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,13 @@
-console.log('Hello World!')
+import express from "express";
+import { config } from 'dotenv';
+import { router } from "./routes";
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server Running on Port: ${ port }`);
+});
